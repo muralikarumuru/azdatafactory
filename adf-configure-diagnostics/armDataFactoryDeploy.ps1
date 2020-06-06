39fb5ae4934fa99f5c1142f1aabdb9798001e719
Murali Karumuru <37239609+muralikarumuru@users.noreply.github.com>
2020-06-06 15:14:05 +0530

    Update armDataFactoryDeploy.ps1

--- adf-configure-diagnostics/armDataFactoryDeploy.ps1
+++ adf-configure-diagnostics/armDataFactoryDeploy.ps1
@@ -5,6 +5,6 @@ $deploymentName = "adfdeployment" + "-" + $deploymentTime
 # Make sure the arm template location is correct
 $armTemplateLocation = (Get-Location).Path + "\azdatafactory\adf-configure-diagnostics\"
 $armTemplateFile = $armTemplateLocation + "azdatafacatory.arm.json"
-$armTemplateParamFile = $armTemplateLocation + "azdatafactory.arm.parameter.dev.json"
+$armTemplateParamFile = $armTemplateLocation + "azdatafactory.arm.parameter.json"
 New-AzureRmResourceGroup -Name $resourceGroup -Location $location
 New-AzureRmResourceGroupDeployment -Name $deploymentName -ResourceGroupName $resourceGroup -TemplateFile $armTemplateFile -TemplateParameterFile $armTemplateParamFile
